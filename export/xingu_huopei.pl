f116b7579c32518a57028a92e982a9de8df3a3c1
Fayland Lam <fayland@gmail.com>
2022-08-30 14:55:18 +0800

    more

--- export/xingu_huopei.pl
+++ export/xingu_huopei.pl
@@ -7,12 +7,10 @@ use FindBin qw/$Bin/;
 use lib "$Bin/../lib";
 use Fund::Utils qw/dbh/;
 use Data::Dumper;
-use Encode;
 use Template;
 
 my $dbh = dbh();
 
-# 根据条件导出数据
 my @rows;
 my $sth = $dbh->prepare("SELECT * FROM xingu_huopei JOIN fund ON fund=code ORDER BY RATIO DESC");
 $sth->execute();
@@ -20,10 +18,6 @@ while (my $row = $sth->fetchrow_hashref) {
     print Dumper(\$row);
     push @rows, $row;
 }
-
-# open(my $fh, '>:utf8', "$Bin/../html/xingu_huopei.html") or die $!;
-# print $fh decode_utf8($html);
-# close($fh);
 
 my $tt2 = Template->new({
     INCLUDE_PATH => $Bin,
